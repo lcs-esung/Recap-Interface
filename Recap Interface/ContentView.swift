1d5bac3e25010974db9e0cd70d423999ba405179
Edna Sung <esung@lcs.on.ca>
2024-01-10 11:46:11 -0500

    Initial Commit

---
+++ Recap Interface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recap Interface
+//
+//  Created by Edna Sung on 2024-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recap Interface/Recap_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recap_InterfaceApp.swift
+//  Recap Interface
+//
+//  Created by Edna Sung on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Recap_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
