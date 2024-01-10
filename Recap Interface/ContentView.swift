d0f8657d49b38b99a6ecf1e382569f5894faabb7
Edna Sung <esung@lcs.on.ca>
2024-01-10 12:26:49 -0500

    Created substructure for the first view(the first line).

--- Recap Interface/ContentView.swift
+++ Recap Interface/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack{
+            Text("My Home")
+
+            HStack {
+                FirstView(title: "Climate", descriptions: "", symbol: <#T##String#>)
+               
+            }
         }
         .padding()
     }
---
+++ Recap Interface/Preview Content/FirstView.swift
@@ -0,0 +1,60 @@
+//
+//  FirstView.swift
+//  Recap Interface
+//
+//  Created by Edna Sung on 2024-01-10.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    
+    let darkGray = Color(
+        hue: 0/360.0,
+        saturation: 0.0,
+        brightness: 0.45
+    )
+    let lightGray = Color(
+        hue: 0/360.0,
+        saturation: 0.0,
+        brightness: 0.73
+    )
+    
+    let title: String
+    let descriptions: String
+    let symbol: String
+    
+    var body: some View {
+        
+        ZStack {
+            Rectangle()
+                .foregroundStyle(darkGray)
+                .frame(width: 150, height: 50)
+                .cornerRadius(30)
+                
+            HStack {
+                Image(systemName: symbol)
+                    .foregroundColor(.cyan)
+                
+                VStack{
+                    
+                    Text(title)
+                        .foregroundStyle(.white)
+                        .padding(.trailing,33)
+                        .bold()
+                    
+                    Text(descriptions)
+                        .foregroundStyle(lightGray)
+                        .font(.system(size: 14))
+                        .padding(.trailing,13)
+                }
+            }
+        }
+        
+    
+    }
+}
+
+#Preview {
+    FirstView(title: "Climate", descriptions: "16.0-20.5C°", symbol: "fan.fill")
+}
